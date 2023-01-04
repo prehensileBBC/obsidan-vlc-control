8bab9f3c6f21f0ac68e41b1606ed4fc3347c0f94
prehensileBBC <henry.cooke@bbc.co.uk>
2023-01-04 11:50:10 +0000

    tidying up

--- main.ts
+++ main.ts
@@ -3,102 +3,54 @@ import { threadId } from 'worker_threads';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface VLCControlSettings {
 	vlcPassword: string;
 	vlcHost: string;
 	vlcPort: string;
 	forcePlayOnSeek: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: VLCControlSettings = {
 	vlcPassword: '',
 	vlcHost: 'http://127.0.0.1',
 	vlcPort: '8080',
 	forcePlayOnSeek: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// regexp to match timestamps in transcript timestamp, uses groups to match minutes and seconds
+const reTs = /([0-9]+):([0-9]+).[0-9]+/;
+
+
+export default class VLCControlPlugin extends Plugin {
+	settings: VLCControlSettings;
 
 	async onload() {
 		await this.loadSettings();
-		/*
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-		*/
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new VLCControlSettingTab(this.app, this));
 
-		/*
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-		*/
-
+		// listen for right-click menu events in an editor window
 		this.registerEvent(
 			this.app.workspace.on("editor-menu", (menu, editor, view) => {
-			  const e = view.editor;
-			  if(!e) return;
-			  const sel = e.getSelection();
-			  const title = "Seek VLC to " + ( sel|| '');
-			  menu.addItem((item) => {
-				item
-				  .setTitle( title )
-				  .setIcon("chevrons-right")
-				  .onClick(() => this.onSeekClick());
-			  });
+				
+				// bail if there isn't a current editor view
+				const e = view.editor;
+			  	if(!e) return;
+				
+				// bail if current selection doesn't contain a timestamp
+				const sel = e.getSelection();
+				if( !reTs.test(sel) ) return;
+
+			  	const title = "Seek VLC to " + ( sel|| '');
+				
+				menu.addItem((item) => {
+					item
+					.setTitle( title )
+					.setIcon( "chevrons-right" )
+					.onClick( () => this.onSeekClick() );
+				});
 	  
 			})
 		  );
@@ -109,14 +61,14 @@ export default class MyPlugin extends Plugin {
 		const vlcHost = this.settings.vlcHost;
 		const vlcPort = this.settings.vlcPort;
 		
+		// construct query string for url
 		const p = new URLSearchParams();
-
 		if( params ){
+			// populate with optional extra parameters
 			for( const k in params ){
 				p.append( k, params[k] )
 			}
 		}
-
 		p.append( 'command', command );
 		const q = p.toString();
 
@@ -139,12 +91,13 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onSeekClick(){
+		
 		let view = this.app.workspace.getActiveViewOfType(MarkdownView);
 		if( !view ) return;
+		
 		let sel = view.editor.getSelection();
 		
-		// regexp to match timestamps in transcript timestamp, uses groups to match minutes and seconds
-		const reTs = /([0-9]+):([0-9]+).[0-9]+/;
+		// check that there's a timestamp in the selection
 		if( reTs.test( sel ) ){
 			
 			const matches = sel.match( reTs );
@@ -196,9 +149,9 @@ class SampleModal extends Modal {
 }
 
 class VLCControlSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: VLCControlPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: VLCControlPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
